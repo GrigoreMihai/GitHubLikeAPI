ec979dd7044bf925f7b3d1b8d0d3605f3149e43c
Grigore Mihai <grigoremihai56@gmail.com>
2019-11-13 23:32:35 +0200

    build: added route repos

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const router = require('./router.js');
+const routerRepos =require('./routerRepos.js');
 const jsonParser = bodyParser.json();
 
 app.use(jsonParser);
 app.use(router);
+app.use(routerRepos);
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
