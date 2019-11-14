f1f7aaa7d8d75f3323b9ee4b8c1aa770826fa269
Grigore Mihai <grigoremihai56@gmail.com>
2019-11-14 17:09:06 +0200

    chore: started post test

--- __tests__/repos.test.js
+++ __tests__/repos.test.js
@@ -1,5 +1,5 @@
 const request = require ("request");
-test("should  work",  done => {	  
+test("should  work get on id",  done => {	  
 	const options = {
 		url: "http://localhost:8000/repos/1" ,
 		method: "get",
@@ -26,6 +26,42 @@ test("should  work",  done => {
 	promise.then(body => {
 		console.log(body);
 		expect(body.name).toBe("come on");
+		done();
+	});    
+
+});
+
+
+test("should  work post",  done => {	  
+	const options = {
+		url: "http://localhost:8000/repos" ,
+		method: "post",
+		body : {
+			"name": "MyRepo",
+			"description": "Good code comments itself",
+			"userEmail" :"some@some.com"
+		}
+	}; 
+	let promise = new Promise((resolve,reject) => {
+		request(options, async function (error, response, body) {
+			if (error) {
+				resolve(false);
+			}
+			if ( typeof response === "undefined") {
+				resolve(false);
+			}    
+			if (response.statusCode !== 200) {
+				resolve(false);
+			}
+			body = body.toString( );
+			body = JSON.parse(body);
+			resolve(body);
+		});
+        
+	});
+	promise.then(body => {
+		console.log(body);
+		expect(body.name).toBe("MyRepo");
 		done();
 	});
     
