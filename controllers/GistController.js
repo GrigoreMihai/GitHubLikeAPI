380e61ef30a1a2d5c6d388482dbb1621070bd49b
Cristi <cris@localhost>
2019-11-14 16:44:59 +0200

    Added gist

---
+++ controllers/GistController.js
@@ -0,0 +1,65 @@
+const models = require('../models');
+
+const GistController = {
+  show: (req, res) => {
+    models
+      .Gist
+      .findByPk(req.params.id)
+      .then(data => {
+        if (!data) {
+          return res.send({});
+        }
+
+        return res.send(data);
+      })
+  },
+  index: (req, res) => {
+    models
+      .Gist
+      .findAll()
+      .then(data => res.send(data));
+
+  },
+  create: (req, res) => {
+    const body = req.body;
+    models
+      .Gist
+      .create({
+        fileName: body.fileName,
+        description: body.description,
+        content: body.content,
+        secret: body.secret,
+      })
+      .then(Gist => {
+        return res.send(gist);
+      });
+  },
+  update: (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+    models
+      .Gist
+      .update(body, { where: { id: id }})
+      .then(updated => {
+        models
+          .Gist
+          .findByPk(id)
+          .then(data => res.send(data));
+      });
+  },
+  delete: (req, res) => {
+    const id = req.params.id;
+    models
+      .Gist
+      .destroy({
+        where: {
+          id: id,
+        }
+      })
+      .then(data => {
+        return res.send(true);
+      })
+  },
+};
+
+module.exports = GistController;
